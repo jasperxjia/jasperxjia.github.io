66299190c2293a97357151bf0a91abf49312d99e
jasperxjia <62466810+jasperxjia@users.noreply.github.com>
2020-05-06 10:42:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ function staticLoadPlaces() {
 function dynamicLoadPlaces(position) {
     let params = {
         radius: 3000,    // search places not farther than this value (in meters)
-        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',   // add your credentials here
-        clientSecret: '',   // add your credentials here
+        clientId: 'YWZGWLNCFMGGIDF5FABJITRA0J04P2TRANBIXLWNHZIAF4A5',   // add your credentials here
+        clientSecret: 'XTKRV2S4FFVCLZRXNJ01T5CQLZLVLQL1W50MGMNIGDSA1YY0',   // add your credentials here
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
