05fca2294e2068f40a4195d39431664783972c1f
jasperxjia <62466810+jasperxjia@users.noreply.github.com>
2020-05-06 07:52:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function staticLoadPlaces() {
 // getting places from REST APIs
 function dynamicLoadPlaces(position) {
     let params = {
-        radius: 300,    // search places not farther than this value (in meters)
+        radius: 1000,    // search places not farther than this value (in meters)
         clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
         clientSecret: '',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
