9d5e4401e79a2d1d8fa227f77c7fee01285548f0
jasperxjia <jasperxjia@users.noreply.github.com>
2020-05-06 16:34:59 +0100

    ada

--- script.js
+++ script.js
@@ -69,9 +69,7 @@ window.onload = () => {
                     const text = document.createElement('a-link');
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
-                    text.setAttribute('url', '_blank');
                     text.setAttribute('scale', '10 10 10');
-
                     text.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
                     });
