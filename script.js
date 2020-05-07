682b66ecc71c3714ed46bdfcf041e5747d8247a3
jasperxjia <jasperxjia@users.noreply.github.com>
2020-05-07 17:29:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -78,8 +78,22 @@ window.onload = () => {
       
 
                     text.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                        
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
+                        //Voice API
+                        var synth = window.speechSynthesis;          
+                        function speak(){
+                            if (synth.speaking) {
+                                console.error('speechSynthesis.speaking');
+                                return;
+                            }
+                            else {
+                            var utterThis = new SpeechSynthesisUtterance(scene.text);
+                            synth.speak(utterThis);
+                            }
+                        } 
+
+                        speak();
+                                            
                     });
 
                     scene.appendChild(text);
@@ -98,19 +112,6 @@ window.onload = () => {
 
 };
 
-    //Voice API
-    var synth = window.speechSynthesis;          
-    function speak(){
-        if (synth.speaking) {
-            console.error('speechSynthesis.speaking');
-            return;
-        }
-        else {
-        var utterThis = new SpeechSynthesisUtterance(scene.text);
-        synth.speak(utterThis);
-        }
-    } 
 
-    speak();
 
                 
