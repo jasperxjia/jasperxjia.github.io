6998bad08064fbd7e7d4e7519b1baa6923cc29bc
jasperxjia <jasperxjia@users.noreply.github.com>
2020-05-11 11:59:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ window.onload = () => {
                    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                    icon.setAttribute('id', place.name);
                    icon.setAttribute('src', './assets/map-marker.png');
-                   icon.setAttribute('event-set__enter','_event: mouseenter; color: #8FF7FF');
+                   
            
                    // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
                    icon.setAttribute('depth', '10');
