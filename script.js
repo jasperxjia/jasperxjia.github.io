f5397abdb91a8a86112d011122b54d5c4053c233
jasperxjia <jasperxjia@users.noreply.github.com>
2020-05-11 19:08:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ window.onload = () => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
 
-              /*      // add place name
+                    // add place name
                     const text = document.createElement('a-link');
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
@@ -84,36 +84,8 @@ window.onload = () => {
                     });
 
                     scene.appendChild(text);
-                    */
                 
-            // add place icon
-            const icon = document.createElement('a-entity');
-            icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-            icon.setAttribute('gltf-model', "#animated-asset");
-            // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-            icon.setAttribute('scale', '0.21 0.21 0.21');
-            icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-            const clickListener = function (ev) {
-               ev.stopPropagation();
-                ev.preventDefault();
-                console.log('Clicked');
-                const name = ev.target.getAttribute('name');
-                const el = ev.detail.intersection && ev.detail.intersection.object.el;
-                if (el && el === ev.target) {
-                    const label = document.createElement('span');
-                    const container = document.createElement('div');
-                    container.setAttribute('id', 'place-label');
-                    label.innerText = name;
-                    container.appendChild(label);
-                    document.body.appendChild(container);
-                    setTimeout(() => {
-                        container.parentElement.removeChild(container);
-                    }, 1500);
-                }
-            };
-            icon.addEventListener('click', clickListener);
-            scene.appendChild(icon);
- 
+
                    
 
                     
