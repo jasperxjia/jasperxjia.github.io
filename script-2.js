e65ff4c3cfc29429e7892c1efbf4d698411bb223
jasperxjia <jasperxjia@users.noreply.github.com>
2020-05-12 06:18:56 +0100

    Update script-2.js

--- script-2.js
+++ script-2.js
@@ -1,37 +1,8 @@
-window.onload = () => {
-    let method = 'dynamic';
+const loadPlaces = function (coords) {
+    // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
+    const method = 'api';
 
-    // if you want to statically add places, de-comment following line
-    method = 'static';
-
-    if (method === 'static') {
-        let places = staticLoadPlaces();
-        renderPlaces(places);
-    }
-
-    if (method !== 'static') {
-
-        // first get current user location
-        return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // than use it to load from remote APIs some places nearby
-            dynamicLoadPlaces(position.coords)
-                .then((places) => {
-                    renderPlaces(places);
-                })
-        },
-            (err) => console.error('Error in retrieving position', err),
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0,
-                timeout: 27000,
-            }
-        );
-    }
-};
-
-function staticLoadPlaces() {
-    return [
+    const PLACES = [
         {
             name: "Your place name",
             location: {
@@ -39,19 +10,18 @@ function staticLoadPlaces() {
                 lng: 0, // add here longitude if using static data
             }
         },
-        {
-            name: 'Another place name',
-            location: {
-                lat: 0,
-                lng: 0,
-            }
-        }
     ];
-}
+
+    if (method === 'api') {
+        return loadPlaceFromAPIs(coords);
+    }
+
+    return Promise.resolve(PLACES);
+};
 
 // getting places from REST APIs
-function dynamicLoadPlaces(position) {
-    let params = {
+function loadPlaceFromAPIs(position) {
+    const params = {
         radius: 300,    // search places not farther than this value (in meters)
         clientId: 'YWZGWLNCFMGGIDF5FABJITRA0J04P2TRANBIXLWNHZIAF4A5', // Bindou Web AR App ID
         clientSecret: 'DJLNEE3G0GKV0Y0KHA1UTMINYQE20PVCDULSWXJR0WAV0SV4', // Bindou Web AR App Secret
@@ -59,15 +29,15 @@ function dynamicLoadPlaces(position) {
     };
 
     // CORS Proxy to avoid CORS problems
-    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
     // Foursquare API
-    let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
         &ll=${position.latitude},${position.longitude}
         &radius=${params.radius}
         &client_id=${params.clientId}
         &client_secret=${params.clientSecret}
-        &limit=15
+        &limit=8
         &v=${params.version}`;
     return fetch(endpoint)
         .then((res) => {
@@ -82,51 +52,67 @@ function dynamicLoadPlaces(position) {
 };
 
 
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+    
 
-    places.forEach((place) => {
-        const latitude = place.location.lat;
-        const longitude = place.location.lng;
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
 
-        // add place icon
-        const icon = document.createElement('a-image');
-        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        icon.setAttribute('name', place.name);
-        icon.setAttribute('src', './assets/place_icon.png');
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
 
-        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale', '20, 20');
+                    // add place name
+                    const icon = document.createElement('a-box');
+                    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    icon.setAttribute('name', place.name);
+                    icon.setAttribute('color', 'yellow');
+                    icon.setAttribute('depth', '10');
+                    icon.setAttribute('height', '10');
+                    icon.setAttribute('width', '10');
+                    icon.setAttribute('look-at', '[gps-camera]');
+                    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+                    
 
-        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+                    /*const clickListener = function (ev) {
+                        ev.stopPropagation();
+                        ev.preventDefault();
+            
+                        let name = ev.target.getAttribute('title');
+            
+                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
+            
+                        if (el && el === ev.target) {
+                            const label = document.querySelector('.log');
+                            label.innerText = name;
+                        }
+                    };*/
+                    
 
-        const clickListener = function (ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
+                    scene.appendChild(icon);
 
-            const name = ev.target.getAttribute('name');
+                    icon.addEventListener('click', () => {
+                        alert("Clicked!");
+                      });
+                   
 
-            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+                    
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
 
-            if (el && el === ev.target) {
-                const label = document.createElement('span');
-                const container = document.createElement('div');
-                container.setAttribute('id', 'place-label');
-                label.innerText = name;
-                container.appendChild(label);
-                document.body.appendChild(container);
-
-                setTimeout(() => {
-                    container.parentElement.removeChild(container);
-                }, 1500);
-            }
-        };
-
-        icon.addEventListener('click', clickListener);
-
-        scene.appendChild(icon);
-    });
-}
 
 
 
