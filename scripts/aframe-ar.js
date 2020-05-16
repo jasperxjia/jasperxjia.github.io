76f7d24268b1e9fd93afe94591eaf7222523262f
jasperxjia <jasperxjia@users.noreply.github.com>
2020-05-16 16:39:05 +0100

    saxsax

--- scripts/aframe-ar.js
+++ scripts/aframe-ar.js
@@ -5154,7 +5154,9 @@ AFRAME.registerComponent('arjs-anchor', {
             _this.el.emit('markerFound')
         } else if (_this._arAnchor.object3d.visible === false && wasVisible === true) {
             _this.el.emit('markerLost')
-        }
+        } else {
+			_this.el.emit('nomarker')
+		}
     }
 })
 
